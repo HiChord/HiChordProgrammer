cf836576a5786d93de8710b90461f8c62f1d881a
HiChord <hello@hichord.shop>
2024-10-24 12:23:00 -0400

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,25 +1,77 @@
 // Generic Strings
 const root_url = "https://electro-smith.github.io/Programmer"
 
-// Data object with only necessary properties
+// New changes involve reading from sources.json to find the 'sources' we should pull from
+// Those sources replace the previously hard coded 'examples.json' file, and should otherwise 
+// function the same.
+
+// The changes should primarily only affect gatherExampleData
+
+// When imported the examples will have the original data located in the .json file
+// as well as the 'source' field containing the data structure used to find the example
+
 var data = { 
+    platforms: [],
+    examples: [],
     no_device: true,
+    sel_platform: null,
+    sel_example: null,
+    firmwareFile: null,
     blinkFirmwareFile: null,
     bootloaderFirmwareFile: null,
+    displayImportedFile: false,
+    displaySelectedFile: false
 }
 
-// Function to get the root URL
+// Global Buffer for reading files
+var ex_buffer
+
+// Gets the root url
+// should be https://localhost:9001/Programmer on local
+// and https://electro-smith.github.io/Programmer on gh-pages
 function getRootUrl() {
     var url = document.URL;
     return url;
 }
 
-// Function to read firmware files from the server
-async function readServerFirmwareFile(path)
+function displayReadMe(fname)
+{
+    var url = self.data.sel_example.url
+    fname   = fname.substring(5,fname.length-4);
+    
+    div = document.getElementById("readme")
+
+    marked.setOptions({
+        renderer: new marked.Renderer(),
+        highlight: function(code, language) {
+            const validLanguage = hljs.getLanguage(language) ? language : 'plaintext';
+            return hljs.highlight(validLanguage, code).value;
+        },
+        pedantic: false,
+        gfm: true,
+        breaks: false,
+        sanitize: false,
+        smartLists: true,
+        smartypants: false,
+        xhtml: false
+    });
+    
+    
+    fetch(url)
+    .then(response => response.text())
+        .then(text => div.innerHTML = marked.parse(text.replace("404: Not Found", "No additional details available for this example.")));
+}
+
+async function readServerFirmwareFile(path, dispReadme = true)
 {
     return new Promise((resolve) => {
+        var buffer
         var raw = new XMLHttpRequest();
         var fname = path;
+    
+        if(dispReadme){
+            displayReadMe(fname)
+        }
     
         raw.open("GET", fname, true);
         raw.responseType = "arraybuffer"
@@ -81,7 +133,14 @@ var app = new Vue({
                             <li><p>Enter the system bootloader by holding the BOOT button down, and then pressing, and releasing the RESET button.</p></li>
                             <li><p>Click the Connect button at the top of the page.</p></li>
                             <li><p>Select "DFU in FS Mode".</p></li>
-                            <li><p>Click the "Flash HiChord Firmware!" button to load the firmware.</p></li>
+                            <li>
+                                <p>Now do either of the following:</p>
+                                <ul>
+                                    <li><p>Flash the HiChord firmware</p></li>
+                                    <li><p>Select a platform and an example from the drop-down menu (if applicable).</p></li>
+                                    <li><p>Click the Choose File button, and select the .bin file you would like to flash.</p></li>
+                                </ul>
+                            </li>
                             <li><p>Click Program, and wait for the progress bar to finish.</p></li>
                             <li><p>If the program does not start immediately, pressing RESET on your device will cause the program to start running.</p></li>
                         </ol>
@@ -109,12 +168,39 @@ var app = new Vue({
             </div>
         </div>
         </b-row>
-        <b-row align="center">
+        <b-row align="between">
             <b-col align="center" class="app_column">
                 <b-container>
                     <b-row class="p-2">
                         <legend>Getting Started? Flash the HiChord firmware!</legend>
                         <div><b-button variant="es" id="blink"  :disabled="no_device">Flash HiChord Firmware!</b-button></div>
+                    </b-row>
+                    <hr>
+                    <b-row class="p-2">
+                        <legend> Or select a platform and a program from the menu below.</legend>
+                        <b-form-select placeholder="Platform" v-model="sel_platform" textContent="Select a platform" id="platformSelector">
+                            <template v-slot:first>
+                                <b-form-select-option :value="null" disabled>-- Platform --</b-form-select-option>
+                            </template>
+                            <option v-for="platform in platforms" :value="platform">{{platform}}</option>
+                        </b-form-select>
+                        <b-form-select v-model="sel_example" id="firmwareSelector" required @change="programChanged">
+                            <template v-slot:first>
+                                <b-form-select-option :value="null" disabled>-- Example --</b-form-select-option>
+                            </template>
+                            <b-form-select-option v-for="example in platformExamples" v-bind:key="example.name" :value="example">{{example.name}}</b-form-select-option>
+                        </b-form-select>
+                    </b-row>
+                    <hr>
+                    <b-row class="p-2">
+                        <legend> Or select a file from your computer</legend>
+                            <b-form-file
+                                id="firmwareFile"
+                                v-model="firmwareFile"
+                                :state="Boolean(firmwareFile)"
+                                placeholder="Choose or drop a file..."
+                                drop-placeholder="Drop file here..."
+                            ></b-form-file>
                     </b-row>
                 </b-container>
             </b-col>
@@ -123,7 +209,7 @@ var app = new Vue({
         <b-col align="center" class="app_column">
         <b-container align="center">
             <legend>Programming Section</legend>
-            <b-button id="download" variant='es' :disabled="no_device || !blinkFirmwareFile"> Program</b-button>
+            <b-button id="download" variant='es' :disabled="no_device || !sel_example"> Program</b-button>
 
             <br> <br>
             <b-button variant="es" v-b-toggle.collapseAdvanced>Advanced...</b-button>
@@ -133,6 +219,14 @@ var app = new Vue({
 
             <div class="log" id="downloadLog"></div>            
             <br><br>
+            <div v-if="sel_example||firmwareFile" >            
+                <div v-if="displaySelectedFile">
+                <!--<h3 class="info">Name: {{sel_example.name}}</h3>-->
+                <!--<li>Description: {{sel_example.description}}</li>-->
+                <!--<h3 class="info">File Location: {{sel_example.filepath}} </h3>-->
+                </div>
+            <br>
+            </div>
             <div><div id = "readme"></div> </div>
         </b-container>
         </b-col>
@@ -142,28 +236,134 @@ var app = new Vue({
     </b-container>
     `,
     data: data,
+    computed: {
+        platformExamples: function () {
+            
+            return this.examples.filter(example => example.platform === this.sel_platform)
+        }
+    },
     created() {
         console.log("Page Created")
     },
     mounted() {
         var self = this
         console.log("Mounted Page")
-
-        // Load custom firmware file for the Flash HiChord Firmware! button
-        var customFirmwareUrl = "https://raw.githubusercontent.com/HiChord/HiChordProgrammer/gh-pages/data/hichord_firmware.bin";
-
-        // Read the custom firmware file
-        readServerFirmwareFile(customFirmwareUrl).then(buffer => {
-            blinkFirmwareFile = buffer;
-        });
-
-        // (Optional) Load the bootloader firmware file if needed
-        var bootloaderUrl = "https://raw.githubusercontent.com/electro-smith/Programmer/gh-pages/data/dsy_bootloader_v5_4.bin";
-        readServerFirmwareFile(bootloaderUrl).then(buffer => {
-            bootloaderFirmwareFile = buffer;
-        });
+        this.importExamples()
     },
     methods: {
-        // No additional methods needed
+        importExamples() {
+            var self = this // assign self to 'this' before nested function calls...
+            var src_url = getRootUrl().split("?")[0].concat("data/sources.json") //need to strip out query string
+            var raw = new XMLHttpRequest();
+            raw.open("GET", src_url, true);
+            raw.responseType = "text"
+            raw.onreadystatechange = function ()
+            {
+                if (this.readyState === 4 && this.status === 200) {
+                    var obj = this.response;
+                    buffer = JSON.parse(obj);
+                    buffer.forEach( function(ex_src) {
+                        // Launch another request with async function to load examples from the 
+                        // specified urls 
+                        // This will fill examples directly, and replace the importExamples/timeout situation.
+                        var ext_raw = new XMLHttpRequest();
+                        ext_raw.open("GET", ex_src.data_url, true);
+                        ext_raw.responseType = "text"
+                        ext_raw.onreadystatechange = function ()
+                        {
+                            // This response will contain example data for the specified source.
+                            if (this.readyState === 4 && this.status === 200) {
+                                var ext_obj = this.response;
+                                ex_buffer = JSON.parse(ext_obj);
+                                const unique_platforms = [...new Set(ex_buffer.map(obj => obj.platform))]
+                                ex_buffer.forEach( function(ex_dat) {
+                                    //  Add "source" to example data
+                                    ex_dat.source = ex_src
+                                    
+                                    self.examples.sort(function (i1, i2){ 
+                                        return i1.name.toLowerCase() < i2.name.toLowerCase() ? -1 : 1
+                                    })
+                                    self.examples.push(ex_dat)
+                                })
+                                unique_platforms.forEach( function(u_plat) {
+                                    if (!self.platforms.includes(u_plat)) {
+                                        self.platforms.push(u_plat)
+                                    }
+                                })
+                            }
+                        }
+                        ext_raw.send(null)
+                    })
+                }
+            }
+            raw.send(null)
+        },
+        programChanged(){
+            var self = this
+
+            // Read new file
+            self.firmwareFileName = self.sel_example.name
+            this.displaySelectedFile = true;
+            var srcurl = self.sel_example.source.repo_url
+            var expath = srcurl.concat(self.sel_example.filepath)
+            readServerFirmwareFile(expath).then(buffer => {
+                firmwareFile = buffer
+            })
+        },
     },
+    watch: {
+        firmwareFile(newfile){
+            firmwareFile = null;
+            this.displaySelectedFile = true;
+            // Create dummy example struct
+            // This updates sel_example to enable the Program button when a file is loaded
+            var new_example = {
+                name: newfile.name,
+                description: "Imported File",
+                filepath: null,
+                platform: null
+            }
+            this.sel_example = new_example;
+            let reader = new FileReader();
+            reader.onload = function() {
+                this.firmwareFile = reader.result;
+                firmwareFile = reader.result;
+            }
+            reader.readAsArrayBuffer(newfile);
+        },
+        examples(){
+            var self = this
+
+            // Load custom firmware file for the Blink button
+            var customFirmwareUrl = "https://raw.githubusercontent.com/HiChord/HiChordProgrammer/gh-pages/data/hichord_firmware.bin";
+
+            // Read the custom firmware file
+            readServerFirmwareFile(customFirmwareUrl, false).then(buffer => {
+                blinkFirmwareFile = buffer;
+            });
+
+            // (Optional) Load the bootloader firmware file if needed
+            var bootloaderUrl = "https://raw.githubusercontent.com/electro-smith/Programmer/gh-pages/data/dsy_bootloader_v5_4.bin";
+            readServerFirmwareFile(bootloaderUrl, false).then(buffer => {
+                bootloaderFirmwareFile = buffer;
+            });
+
+            // Parse the query strings
+            var searchParams = new URLSearchParams(getRootUrl().split("?")[1])
+            
+            var platform = searchParams.get('platform')
+            var name = searchParams.get('name')
+            if(platform != null && self.examples.filter(ex => ex.platform === platform)){
+                self.sel_platform = platform
+
+                if(name != null){
+                    var ex = self.examples.filter(ex => ex.name === name && ex.platform === platform)[0]
+                    if(ex != null){
+                        self.sel_example = ex
+                        this.programChanged()
+                    }    
+                }
+            }
+        }
+    }
 })
